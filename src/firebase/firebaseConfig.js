ca51d636ebcb6cf5c737fd180dde09a5c08a689e
vaishnavi150 <102913677+vaishnavi150@users.noreply.github.com>
2024-12-13 14:56:42 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1811cSMnjniIl8S-e5qm0P3ovLKo2yw8",
-  authDomain: "vibesnap-788b9.firebaseapp.com",
-  projectId: "vibesnap-788b9",
-  storageBucket: "vibesnap-788b9.firebasestorage.app",
-  messagingSenderId: "875286180565",
-  appId: "1:875286180565:web:ccdb2610a6443038a2a85c",
-  measurementId: "G-92H760SB0C"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
